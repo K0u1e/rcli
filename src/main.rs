e92994919d9deb90fd569a4dd6a78cccdf4c4671
K0u1e <koule2333@gmail.com>
2024-04-21 22:55:07 +0800

    Add args parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::path::Path;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author, about, long_about = None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+enum SubCommand {
+    #[command(name = "csv", about = "Show CSV, or convert CSV to other format")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input_file)]
+    input: String,
+
+    #[arg(short, long, default_value = "output.json")] // "output.json".into()
+    output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
+fn verify_input_file(filename: &str) -> Result<String, &'static str> {
+    if Path::new(filename).exists() {
+        Ok(filename.into())
+    } else {
+        Err("File does not exist")
+    }
+}
+
 fn main() {
-    println!("Hello, rust!");
+    let opts = Opts::parse();
+    println!("{:?}", opts);
 }
