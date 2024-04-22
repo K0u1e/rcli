a90087e53d5507891c4e76e3c66a9334eb7c251f
K0u1e <koule2333@gmail.com>
2024-04-22 23:49:32 +0800

    Simply refactor rcli

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,9 @@
 use clap::Parser;
-use csv::Reader;
-use serde::{Deserialize, Serialize};
-use std::{fs, path::Path};
-#[derive(Debug, Deserialize, Serialize)]
-struct Player {
-    #[serde(rename = "Name")]
-    name: String,
-    #[serde(rename = "Position")]
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    #[serde(rename = "Nationality")]
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
-
-#[derive(Debug, Parser)]
-#[command(name = "rcli", version, author, about, long_about = None)]
-struct Opts {
-    #[command(subcommand)]
-    cmd: SubCommand,
-}
-
-#[derive(Debug, Parser)]
-enum SubCommand {
-    #[command(name = "csv", about = "Show CSV, or convert CSV to other format")]
-    Csv(CsvOpts),
-}
-
-#[derive(Debug, Parser)]
-struct CsvOpts {
-    #[arg(short, long, value_parser = verify_input_file)]
-    input: String,
-
-    #[arg(short, long, default_value = "output.json")] // "output.json".into()
-    output: String,
-
-    #[arg(short, long, default_value_t = ',')]
-    delimiter: char,
-
-    #[arg(long, default_value_t = true)]
-    header: bool,
-}
-
-fn verify_input_file(filename: &str) -> Result<String, &'static str> {
-    if Path::new(filename).exists() {
-        Ok(filename.into())
-    } else {
-        Err("File does not exist")
-    }
-}
+use rcli::{process_csv, Opts, SubCommand};
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     match opts.cmd {
-        SubCommand::Csv(opts) => {
-            let mut reader = Reader::from_path(opts.input)?;
-            let mut ret = Vec::with_capacity(128);
-            for result in reader.deserialize() {
-                let record: Player = result?;
-                ret.push(record);
-            }
-            let json = serde_json::to_string_pretty(&ret)?;
-            fs::write(opts.output, json)?;
-            Ok(())
-        }
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output),
     }
 }
